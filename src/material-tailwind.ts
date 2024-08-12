01189e1f31e4f8d6c5efb9bc482d49d596140348
Mohsin Hassan <86055307+mohsinparvi@users.noreply.github.com>
2024-08-12 11:23:15 +0500

    feat: fix deployment issues

--- src/material-tailwind.ts
+++ src/material-tailwind.ts
@@ -6,6 +6,22 @@ import {
   Button,
   Typography,
   IconButton,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+  Avatar,
 } from "@material-tailwind/react";
 
-export { ThemeProvider, Drawer, Button, Typography, IconButton };
+export {
+  ThemeProvider,
+  Drawer,
+  Button,
+  Typography,
+  IconButton,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+  Avatar,
+};
