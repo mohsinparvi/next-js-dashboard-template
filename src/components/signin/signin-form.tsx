176c75798ce0c123020f5349fca1c0f4c0b7501b
Mohsin Hassan <86055307+mohsinparvi@users.noreply.github.com>
2024-08-12 11:29:17 +0500

    feat: login structure

--- src/components/signin/signin-form.tsx
+++ src/components/signin/signin-form.tsx
@@ -1,20 +1,40 @@
+"use client";
 import InputLabel from "@/components/utils/input-label";
 import Input from "@/components/utils/input";
 
 import { Button } from "@/material-tailwind";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 
 const SigninForm = () => {
+  const [formData, setFormData] = useState({
+    email: "mohsin@gmail.com",
+    password: "mohsin$123@*",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSignIn = () => {
+    //TODO: Implement sign-in logic here
+    // Redirect to dashboard upon successful login\
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+      window.location.href = "/dashboard";
+    }, 3000);
+  };
   return (
     <form className="space-y-4 ">
       <div className="space-y-2">
         <InputLabel label="Email Address" />
-        <Input type="email" placeholder="Email Address" />
+        <Input
+          type="email"
+          placeholder="Email Address"
+          value={formData.email}
+        />
       </div>
       <div className="space-y-2">
         <InputLabel label="Password" />
-        <Input type="text" placeholder="••••••••" />
+        <Input type="text" placeholder="••••••••" value={formData.password} />
       </div>
       <Button
         placeholder={""}
@@ -24,6 +44,8 @@ const SigninForm = () => {
         size="md"
         ripple
         className="w-full rounded text-sm capitalize font-medium"
+        loading={isLoading}
+        onClick={handleSignIn}
       >
         Sign In
       </Button>
